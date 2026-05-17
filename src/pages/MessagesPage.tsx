215e3dd64f2ccedccc1fe5862917885232fbd0cc
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2026-05-17 21:51:34 +1000

    chore: add placeholder sections to MessagesPage
    just to mentally understand what needs to go where

--- src/pages/MessagesPage.tsx
+++
@@ -1,3 +0,0 @@
-const MessagesPage = () => <>All messages here</>;
-
-export default MessagesPage;
---
+++ src/pages/MessagesPage/index.tsx
@@ -0,0 +1,14 @@
+const MessagesPage = () => (
+  <main>
+    {/* expandable drawer */}
+    <nav></nav>
+
+    {/* list of messages */}
+    <section></section>
+
+    {/* currently opened message */}
+    <section></section>
+  </main>
+);
+
+export default MessagesPage;
