a0103a7e81bb9b9537f7eb7ab9dd13d2000bd77a
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2026-05-18 15:36:24 +1000

    feat(MessagePreview): prevent text from wrapping. ensure name of author truncates and doesn't create new line when screen gets narrower. display time as relative time

--- src/pages/MessagesPage/MessagesList/MessagePreview/index.tsx
+++ src/pages/MessagesPage/MessagesList/MessagePreview/index.tsx
@@ -1,33 +1,55 @@
 import { Avatar, Group, Stack, Text } from "@mantine/core";
 import type { Message } from "@models/messages";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+import updateLocale from "dayjs/plugin/updateLocale";
 import { Link } from "react-router";
 
 interface Props {
   message: Message;
 }
 
-const MessagePreview = ({ message }: Props) => (
-  <Link to={`/messages/${message.author.id}`}>
-    <Group gap="sm">
-      <Avatar
-        src={message.author.avatarSrc}
-        alt={`Avatar of ${message.author.name}`}
-      />
-      <Stack gap={0}>
-        <Group justify="space-between">
-          <Text fz="md" fw="bold">
-            {message.author.name}
+const MessagePreview = ({ message }: Props) => {
+  const { author, text, sentTime } = message;
+
+  dayjs.extend(relativeTime);
+  dayjs.extend(updateLocale);
+
+  dayjs.updateLocale("en", {
+    relativeTime: {
+      s: "Just now",
+      m: "1m",
+      mm: "%dm",
+      h: "1h",
+      d: "Yesterday",
+    },
+  });
+
+  const timeSinceLastMessage = dayjs(sentTime).fromNow();
+
+  return (
+    <Link to={`/messages/${author.id}`}>
+      <Group justify="space-between" wrap="nowrap">
+        <Avatar
+          src={message.author.avatarSrc}
+          alt={`Avatar of ${author.name}`}
+        />
+        <Stack gap={0} w="100%">
+          <Group justify="space-between" grow wrap="nowrap">
+            <Text fz="md" fw="bold" lineClamp={1}>
+              {author.name}
+            </Text>
+            <Text c="dimmed" fz="sm" ta="right">
+              {timeSinceLastMessage}
+            </Text>
+          </Group>
+          <Text c="gray.6" fz="sm" lineClamp={1}>
+            {text}
           </Text>
-          {/* <Text c="dimmed" fz="sm">
-            {String(message.sentTime)}
-          </Text> */}
-        </Group>
-        <Text c="gray.6" fz="sm" lineClamp={1}>
-          {message.text}
-        </Text>
-      </Stack>
-    </Group>
-  </Link>
-);
+        </Stack>
+      </Group>
+    </Link>
+  );
+};
 
 export default MessagePreview;
