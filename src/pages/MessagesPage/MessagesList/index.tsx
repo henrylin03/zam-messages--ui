54982bb6160687d962ca73332227e139d99c2480
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2026-05-18 12:59:05 +1000

    feat(MessagesList): add some minor styling and header (title and button) and pass in fake data about list of messages

--- src/pages/MessagesPage/MessagesList/index.tsx
+++ src/pages/MessagesPage/MessagesList/index.tsx
@@ -1,9 +1,23 @@
+import { Button, Group, Stack, Title } from "@mantine/core";
 import type { Message } from "@models/messages";
+import styles from "./MessagesList.module.css"
 
 interface Props {
   messages: Message[];
 }
 
-const MessagesList = ({ messages }: Props) => <></>;
+const MessagesList = ({ messages }: Props) => (
+  <section className={styles.section}>
+    <Group justify="space-between" component="header">
+      <Title order={2}>Messages</Title>
+      <Button>New message</Button>
+    </Group>
+    <Stack component="ul" gap="sm">
+      {messages.map((message) => (
+        <li>{message.author.name}</li>
+      ))}
+    </Stack>
+  </section>
+);
 
 export default MessagesList;
--- src/pages/MessagesPage/index.tsx
+++ src/pages/MessagesPage/index.tsx
@@ -1,16 +1,31 @@
+import type { Message } from "@models/messages";
 import styles from "./MessagesPage.module.css";
+import MessagesList from "./MessagesList";
 
-const MessagesPage = () => (
-  <main className={styles.main}>
-    {/* expandable drawer */}
-    <nav>m</nav>
+const MessagesPage = () => {
+  const FAKE_MESSAGES: Message[] = [
+    {
+      author: {
+        id: "CH",
+        name: "Courtney Henry",
+        avatarSrc: "https://i.pravatar.cc/100",
+      },
+      text: "What time do you close?",
+      sentTime: new Date(),
+    },
+  ];
 
-    {/* list of messages */}
-    <section>Messages</section>
+  return (
+    <main className={styles.main}>
+      {/* expandable drawer */}
+      <nav>m</nav>
 
-    {/* currently opened message */}
-    <section>Brooklyn Simmons</section>
-  </main>
-);
+      <MessagesList messages={FAKE_MESSAGES} />
+
+      {/* currently opened message */}
+      <section>Brooklyn Simmons</section>
+    </main>
+  );
+};
 
 export default MessagesPage;
